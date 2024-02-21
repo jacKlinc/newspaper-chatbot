5a2c1e8b91cea545abd4e9c7d92281b89fda3c3d
jacKlinc <jack@inne.io>
2024-02-21 13:00:30 +0000

    Query ChromaDB works. Dependencies FINALLY work

--- app/src/constants.py
+++ app/src/constants.py
@@ -1,3 +1,2 @@
 CSV_FILE = "https://gist.githubusercontent.com/jacKlinc/8b71c404c9f8545d33daf20bb4555fd3/raw/ce55f5eb33c32bac73d768ba5193ea5c4cc6aef8/all-bellingcat-articles.csv"
 COLLECTION_NAME = "all-bellingcat-articles"
-EMBEDDINGS_MODEL = "nateraw/bge-large-en-v1.5:9cf9f015a9cb9c61d1a2610659cdac4a4ca222f2d3707a68517b18c198a9add1"
--- app/src/pages/_1_bellingcat_titles.py
+++ app/src/pages/_1_bellingcat_titles.py
@@ -1,21 +1,42 @@
 import json
 import os
+from typing import Any
+
+# fixes chromadb issue
+__import__("pysqlite3")
+import sys
+
+sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
 
 import streamlit as st
 import pandas as pd
 from replicate import Client
 from dotenv import load_dotenv
+import chromadb
+from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 
 from ..types import Page, Article
-from ..constants import CSV_FILE, EMBEDDINGS_MODEL, COLLECTION_NAME
+from ..constants import CSV_FILE, COLLECTION_NAME
 
 load_dotenv(override=True)
 
+
 REPLICATE_API_TOKEN = os.getenv("REPLICATE_API_TOKEN")
+sentence_transformer_ef = SentenceTransformerEmbeddingFunction(
+    model_name="all-MiniLM-L6-v2"
+)
 
 
 class Bellingcat(Page):
-    replicate_client = Client(api_token=REPLICATE_API_TOKEN)
+    articles: list[Article]
+    replicate_client: Client
+
+    def __init__(self):
+        # Initialize the chromadb directory, and client.
+        self.chroma_client = chromadb.Client()
+        self.replicate_client = Client(
+            api_token=REPLICATE_API_TOKEN, embedding_function=sentence_transformer_ef
+        )
 
     def write(self):
         st.title(self.__class__.__name__)
@@ -25,8 +46,19 @@ class Bellingcat(Page):
         st.dataframe(df)
 
         st.write("## Convert to JSON")
-        articles = self.convert_to_json(df)
-        st.dataframe(articles[0])
+        self.convert_to_json(df)
+        st.dataframe(self.articles[0])
+
+        st.write("## Create Vector Database w ChromaDB")
+        self.collection = self.chroma_client.get_or_create_collection(
+            name=COLLECTION_NAME
+        )
+        self.fill_chroma_collection()
+
+        prompt = st.chat_input("Enter prompt...")
+        if prompt:
+            result = self.query_collection(prompt)
+            st.write(result)
 
     def download_csv(self, csv_file: str) -> pd.DataFrame:
         df = pd.read_csv(csv_file)
@@ -35,5 +67,25 @@ class Bellingcat(Page):
         df["id"] = df.index
         return df
 
-    def convert_to_json(self, df: pd.DataFrame) -> list[Article]:
-        return json.loads(df.to_json(orient="records"))
+    def convert_to_json(self, df: pd.DataFrame) -> None:
+        self.articles = json.loads(df.to_json(orient="records"))
+
+    def fill_chroma_collection(self, batch_size: int = 250) -> None:
+        for i in range(0, len(self.articles), batch_size):
+            batch = self.articles[i : i + batch_size]
+
+            batch_titles = [story["title"] for story in batch]
+
+            # Upsert all of the embeddings, ids, metadata, and title strings into Chromadb.
+            self.collection.upsert(
+                ids=[str(story["id"]) for story in batch],
+                metadatas=[dict(time=story["publish_date"]) for story in batch],
+                documents=batch_titles,
+                embeddings=sentence_transformer_ef(batch_titles),
+            )
+
+    def query_collection(self, query: str, n_results: int = 10) -> str | None:
+        results = self.collection.query(query_texts=[query], n_results=n_results)
+
+        if results:
+            return "\n".join(results["documents"][0])
