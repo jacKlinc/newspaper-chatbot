4e40b9a6d87e8c40660baddf7958f6abc3ef6c37
jacKlinc <jack@inne.io>
2024-03-12 12:50:40 +0000

    Changed to JSON download speeding things up. Cached both the file and the vector database

--- app/src/constants.py
+++ app/src/constants.py
@@ -1,4 +1,4 @@
-CSV_FILE = "https://gist.githubusercontent.com/jacKlinc/8b71c404c9f8545d33daf20bb4555fd3/raw/ce55f5eb33c32bac73d768ba5193ea5c4cc6aef8/all-bellingcat-articles.csv"
+JSON_FILE = "https://gist.githubusercontent.com/jacKlinc/150db5d22b83a0109c831557fb6a89b0/raw/d5213b403829128ef24000e37f9f99ee49c3a782/all-articles.json"
 COLLECTION_NAME = "all-bellingcat-articles"
 MISTRAL_URL = "a16z-infra/mistral-7b-instruct-v0.1:83b6a56e7c828e667f21fd596c338fd4f0039b46bcfa18d973e8e70e455fda70"
 EMBEDDING_FUNCTION = "all-MiniLM-L6-v2"
